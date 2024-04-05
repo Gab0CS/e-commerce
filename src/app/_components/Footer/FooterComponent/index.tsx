5ee56e429953886f775f65d9791b065832400b7b
Gab0CS <gabocanseco@gmail.com>
2024-04-05 11:56:57 -0600

    Footer finished

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,14 +1,17 @@
 "use client"
 import React from 'react'
 import classes from './index.module.scss'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Footer, Media } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
 
-const FooterComponent = () => {
+const FooterComponent = ({ footer } : { footer: Footer}) => {
   const pathname = usePathname();
+  const navItems = footer?.navItems || [];
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? 
@@ -41,6 +44,30 @@ const FooterComponent = () => {
                 height={50}
               />
             </Link>
+            <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+                
+                return(
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinks}
+                  >
+                    <Image 
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
           </div>
         </Gutter>
       </div>
